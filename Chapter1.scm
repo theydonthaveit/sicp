2d42a469a78f644513689bf710833a42aabdd1d2
Alan Williams <theydonthaveit@gmail.com>
2018-11-06 09:51:52 +0000

    exec 1.6

--- Chapter1.scm
+++ Chapter1.scm
@@ -76,3 +76,26 @@ EXERCISE 1.1.7
     (sqrt-iter 1.0 x))
 
 EXERCISE 1.6
+(define (new-if predicate then-clause else-clause)
+    (cond (predicate then-clause)
+        (else else-clause)))
+
+
+(define (sqrt-iter guess x)
+    (new-if (good-enough? guess x)
+            guess
+            (sqrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+    (average guess (/ x guess)))
+
+(define (average x y)
+    (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+    (sqrt-iter 1.0 x))
+; infinite loop, max recursion depth exceeded
+; cond will constantly call sqrt-iter
