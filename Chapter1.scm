1189135c6112ad8ff83bbf896a00361f7929dff0
Alan Williams <theydonthaveit@gmail.com>
2018-11-07 09:37:14 +0000

    exec 1.7

--- Chapter1.scm
+++ Chapter1.scm
@@ -70,10 +70,23 @@ EXERCISE 1.1.7
     (/ (+ x y) 2))
 
 (define (good-enough? guess x)
-    (< (abs (- (square guess) x)) 0.001))
+    (< (abs (- (square guess) x)) (* guess 0.001)))
 
 (define (sqrt x)
     (sqrt-iter 1.0 x))
+
+; sqrt 3
+; sqrt-iter 1.0 3
+;  - good-enough? 1.0 3
+;   - (square 1.0) - 3 = -2
+;   - (abs -2)
+;   - (< 2 0.001)
+;  - sqrt-iter (improve 1.0 3) 3)
+;   - improve 1.0 3 = 1.5
+;    - average 1.0 (3 / 1.0) = average (1.0 3)
+;     - (1.0 + 3) / 2 = 1.5
+;  - sqrt-iter 1.5 3
+
 
 EXERCISE 1.6
 (define (new-if predicate then-clause else-clause)
@@ -99,3 +112,10 @@ EXERCISE 1.6
     (sqrt-iter 1.0 x))
 ; infinite loop, max recursion depth exceeded
 ; cond will constantly call sqrt-iter
+
+EXERCISE 1.7
+(define (good-enough? guess x)
+    (= guess (improve guess x)))
+
+; Machine epsilon gives an upper bound on the relative error due to rounding in floating point arithmetic.
+; Also, in real computers, arith-metic operations are almost always performed with lim-ited precision.
